56d779b73190bb9aa6130ac6bd509dad442462f0
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-10-31 02:55:13 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -5,8 +5,8 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.string :name
       t.text :location
       t.integer :costume_inventory
-      t.integer :employees_count
-      t.boolean :is_still_in_business?
+      t.integer :num_of_employees
+      t.boolean :still_in_business
       t.datetime :opening_time
       t.datetime :closing_time
     end
